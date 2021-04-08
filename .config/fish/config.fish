810c42fd5f1d5b53618b280746281eaf0c1066b2
k07e <sakk7e+dev@gmail.com>
2021-04-08 19:41:55 +0900

    clean .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
 # ref: https://github.com/danhper/fundle#automatic-install
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
+# prompt
 fundle plugin jorgebucaran/hydro
+
+# auto-complete matching pairs
 fundle plugin jorgebucaran/autopair.fish
 
 fundle init
