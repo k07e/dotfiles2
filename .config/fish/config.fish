298bb572d7b9f349aca3800e33b1a7144cc0ba25
k07e <sakk7e+dev@gmail.com>
2021-04-08 19:34:39 +0900

    add .config/fish/config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+# ref: https://github.com/danhper/fundle#automatic-install
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+fundle plugin jorgebucaran/hydro
+
+fundle init
