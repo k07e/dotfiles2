0be70416448e75d10d4a5e749e8417482fe79d64
k07e <sakk7e+dev@gmail.com>
2021-04-18 12:32:18 +0900

    add emacs alias to use 24-bit color

--- .bash_aliases
+++ .bash_aliases
@@ -1,1 +1,2 @@
 alias map='cat -n | while read'
+alias emacs='env TERM=xterm-direct emacs'
--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,1 +1,2 @@
 alias clear 'command clear; echo; echo; seq 1 (tput cols) | sort -R | spark; echo; echo'
+alias emacs 'env TERM=xterm-direct emacs'
