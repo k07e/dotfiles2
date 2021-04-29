079c19751d2b07c0d6f6e498cb14988d549dc25e
k07e <sakk7e+dev@gmail.com>
2021-04-30 08:00:28 +0900

    :truck: change alias to script

--- .bash_aliases
+++ .bash_aliases
@@ -1,6 +1,5 @@
 alias map='cat -n | while read'
 alias emacs='env TERM=xterm-direct emacs'
-alias todo-edit='editor ~/todo.txt'
 alias e=editor
 alias ie=instant-edit
 alias sponge=sponge-wrapper
--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,6 +1,5 @@
 alias clear 'command clear; echo; echo; seq 1 (tput cols) | sort -R | spark; echo; echo'
 alias emacs 'env TERM=xterm-direct emacs'
-alias todo-edit 'editor ~/todo.txt'
 alias l 'ls -CF'
 alias la 'ls -A'
 alias ll 'ls -alF'
