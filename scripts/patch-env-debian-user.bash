143de6e29195f5eb930e918a3b1d13afef9eefaa
k07e <sakk7e+dev@gmail.com>
2021-05-13 20:06:20 +0900

    :art: update to use passwd instead of chpasswd

--- scripts/patch-env-debian-user.bash
+++ scripts/patch-env-debian-user.bash
@@ -1,10 +1,9 @@
 tab=$'\t'
 read -p 'input username: ' username
-read -sp 'input password: ' password
 ./start-debian.sh << end
 apt-get install -y sudo
 useradd -m $username -s /bin/bash
-echo "$username:$password" | chpasswd
+passwd $username
 echo "$username${tab}ALL=(ALL:ALL) ALL" >/etc/sudoers.d/$username
 end
 sed -i "s:\\(HOME=\\|-w \\)/root:\\1/home/$username:;s/\\(bash --login\\)/sudo --user $username \1/" start-debian.sh
