595fa208afbdc2b479a8dd2eeb17ee64bcd5912a
k07e <sakk7e+dev@gmail.com>
2021-05-13 21:31:12 +0900

    :sparkles: add script to japanize

---
+++ scripts/patch-env-debian-japanize.bash
@@ -0,0 +1,8 @@
+./start-debian.sh << end
+{
+  apt-fast install -y locales task-japanese
+  sudo sed -i 's/^# \(ja_JP.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+  sudo /usr/sbin/locale-gen
+} </dev/tty
+end
+sed -i 's/\(LANG=\)C.UTF-8/\1ja_JP.UTF-8/' start-debian.sh
