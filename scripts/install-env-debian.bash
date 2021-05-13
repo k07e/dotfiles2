205579e1e34401df57726b4a022d5491b0addf73
k07e <sakk7e+dev@gmail.com>
2021-05-13 20:03:02 +0900

    :art: update to set $TERM to xterm-256color

--- scripts/install-env-debian.bash
+++ scripts/install-env-debian.bash
@@ -2,6 +2,7 @@ pkg install wget openssl-tool proot -y &&
   hash -r &&
   wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Debian/debian.sh &&
   bash debian.sh
+sed -i 's/\(TERM=\)$TERM/\1xterm-256color/' start-debian.sh
 ./start-debian.sh << end
 apt-get update
 apt-get upgrade -y
