9f61ef4d0d1a8468587491ef798f7ce8cb03133b
k07e <sakk7e+dev@gmail.com>
2021-05-14 17:18:35 +0900

    :fire: remove code that is no needed

--- scripts/install-env-debian.bash
+++ scripts/install-env-debian.bash
@@ -8,7 +8,6 @@ sed -i 's/\(TERM=\)$TERM/\1xterm-256color/' start-debian.sh
   apt-get update
   apt-get upgrade -y
   apt-get install -y apt-utils
-  apt-get install -y debconf
   apt-get install -y dialog
 } </dev/tty
 end
