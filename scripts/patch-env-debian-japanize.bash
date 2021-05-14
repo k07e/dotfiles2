60464d9632e891d6c749357a93b5b06ceebd1985
k07e <sakk7e+dev@gmail.com>
2021-05-14 12:19:30 +0900

    :art: update to able to changing timezone

--- scripts/patch-env-debian-japanize.bash
+++ scripts/patch-env-debian-japanize.bash
@@ -3,6 +3,7 @@
   apt-fast install -y locales task-japanese
   sudo sed -i 's/^# \(ja_JP.UTF-8 UTF-8\)/\1/' /etc/locale.gen
   sudo /usr/sbin/locale-gen
+  sudo dpkg-reconfigure tzdata
 } </dev/tty
 end
 sed -i 's/\(LANG=\)C.UTF-8/\1ja_JP.UTF-8/' start-debian.sh
